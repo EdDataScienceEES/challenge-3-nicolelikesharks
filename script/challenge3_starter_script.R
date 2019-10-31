1007ecf4816106acc32edbd9bbdc5ee77fdae79a
imyerssmith@gmail.com <imyerssmith@gmail.com>
2019-10-31 14:27:02 +0000

    Isla has added the challenge files

---
+++ script/challenge3_starter_script.R
@@ -0,0 +1,30 @@
+# Challenge 3 Statistical Modelling
+# Data Science in EES 2019
+# Starter script written by Isla Myers-Smith
+# 31st October 2019
+
+# Starter code ----
+
+# Libraries
+library(tidyverse)
+library(scales)
+
+# Load Living Planet Data
+LPI_species <- read.csv("data/LPI_species.csv")
+
+# Choose your species from this list - don't forget to register your species
+# on the issue for the challenge and check that no other student has chosen 
+# the species already. Every student must choose a different species!
+unique(LPI_species$Common.Name)
+
+# Filter your species here
+
+# Statistical analysis ----
+
+# Frequentist Linear model ----
+
+# Frequentist Hierarchical linear model ----
+
+# Bayesian linear model ----
+
+# Bayesain hierarchical linear model ----
